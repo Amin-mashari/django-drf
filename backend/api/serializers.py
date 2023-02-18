3c5b80f62b23f422569a9ede538fdfcadeb583f3
Amin-mashari <amin0916.m@gmail.com>
2023-02-18 20:12:13 +0330

    add userserializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 from blog.models import Article
+from django.contrib.auth.models import User
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
+     #    fields = ('title','slug','author','content','publish','status')
+     #    exclude = ('created','updated') #all expect 
+        fields = '__all__' #all models fields
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
      #    fields = ('title','slug','author','content','publish','status')
      #    exclude = ('created','updated') #all expect 
         fields = '__all__' #all models fields
