a9568aa05841dc173f3b7df8d5bc28d5db42bb0e
Amin-mashari <amin0916.m@gmail.com>
2023-02-18 15:11:32 +0330

    add view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from blog.models import Article
+from .serializers import ArticleSerializer
 
 # Create your views here.
+class ArticleList(ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    
