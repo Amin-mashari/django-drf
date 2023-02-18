60f274acdc3341a0fa5e30c7dad9f310883fd085
Amin-mashari <amin0916.m@gmail.com>
2023-02-18 15:27:20 +0330

    set view for article in blog

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import ArticleListView,ArticleDetail
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='list'),
+    path('<int:pk>', ArticleDetail.as_view(), name='detail'),
 ]
