ddf7f15dbb01723095df2d2699c29c1435a40fcc
Amin-mashari <amin0916.m@gmail.com>
2023-02-25 10:46:17 +0330

    add authtocken app

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
     'blog.apps.BlogConfig',
     'api.apps.ApiConfig',
 ]
@@ -134,9 +135,9 @@ REST_FRAMEWORK = {
 
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
-        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         # jwt
         # knox
         # oauth
