00dadbfb72f0868d82ff9a4f048224764f00efad
Amin-mashari <amin0916.m@gmail.com>
2023-02-26 10:50:00 +0330

    add revokeToken and use TokenAuthorization

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,8 @@
 # from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from blog.models import Article
 from .permissions import IsSuperUserOrStaffReadOnly, IsAuthorOrReadOnly , IsStaffOrReadOnly
@@ -30,3 +32,26 @@ class UserDetail(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsSuperUserOrStaffReadOnly,)
+
+
+class RevokeToken(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        return Response({"method":"get"})
+
+    def post(self, request):
+        return Response({"method":"post"})
+
+    def put(self, request):
+        return Response({"method":"put"})
+    
+    def delete(self, request):
+        return Response({"method":"delete"})
+
+
+    # def delete(self ,request):
+    #     request.auth.delete()
+    #     return Response(status=204)
+
+    
